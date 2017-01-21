d4e618f6dcbeba159b6995ca5f6c240b7809b427
Adam Streck <adam.streck@gmail.com>
2017-01-21 20:13:29 +0100

    Small changes.

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    [NotNull]
+    public ViveController left;
+
+    [NotNull]
+    public ViveController right;
+
+    [NotNull]
+    public Transform eye;
+
+    public float forwardSpeed;
+
+    public float downwardSpeed;
+
+    public float turnSpeed;
+
+    public float strafeSpeed;
+
+    public bool turn;
+
+    public float RotateRat;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        float diff = left.transform.position.y - right.transform.position.y;
+        Debug.Log("diff " + diff);
+
+        transform.Translate(Vector3.down * Time.deltaTime * downwardSpeed);
+        transform.Translate(transform.forward * Time.deltaTime * forwardSpeed);
+        if (turn)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + (Time.deltaTime * turnSpeed * diff),  diff * RotateRat));
+        }
+        else
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * strafeSpeed * diff);
+        }
+
+    }
+}
