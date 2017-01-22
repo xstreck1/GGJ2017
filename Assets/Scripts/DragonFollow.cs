64c8623dc584bd033ea4bfbabe6abc9c2bec8124
Adam Streck <adam.streck@gmail.com>
2017-01-22 14:29:54 +0100

    Adddd the sound loop.

--- Assets/Scripts/DragonFollow.cs
+++ Assets/Scripts/DragonFollow.cs
@@ -1,20 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class DragonFollow : MonoBehaviour
 {
+    [NotNull]
+    public TextMesh gui;
+
     public GameObject eye;
     public Vector3 correction;
     public float heightDiff;
 
+    public bool CanMove { get { return !(timer < 0 || timer > totalTime || collided);  } }
+
+    const int pointInc = 50;
+
+
+    bool collided;
+    float timer;
+    const float totalTime = 260f;
+
+    int points = 0;
+
     void Start()
     {
+        collided = false;
+        timer = -5f;
     }
 
     void Update()
     {
+
+
         transform.localPosition = eye.transform.localPosition;
         transform.localPosition += Vector3.down * eye.transform.localPosition.y * heightDiff;
         transform.Translate(correction);
+
+        timer += Time.deltaTime;
+        if (!CanMove)
+        {
+            if (timer < 0)
+            {
+                gui.text = "wave your hands in " + Convert.ToInt32(0f - timer);
+            }
+            else if (timer > totalTime)
+            {
+                gui.text = "final score " + points;
+            }
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            return;
+        }
+        else
+        {
+            gui.text = points.ToString();
+        }
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided");
+        collided = true;
+        gui.text = "CRASHED";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "loop")
+            points += pointInc;
+        else
+        {
+
+            Debug.Log("Collided");
+            collided = true;
+            gui.text = "CRASHED";
+        }
     }
 }
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,10 +15,7 @@ public class Move : MonoBehaviour {
     public Transform eye;
 
     [NotNull]
-    public Transform dragon;
-
-    [NotNull]
-    public TextMesh gui;
+    public DragonFollow dragon;
 
     public float forwardSpeed;
 
@@ -35,81 +31,38 @@ public class Move : MonoBehaviour {
 
     public float waveRiseRatio;
 
-    bool collided;
-    float timer;
-    const float totalTime = 90f;
-
-    int points = 0;
-    const int pointInc = 50;
-
-    void Start () {
-        collided = false;
-        timer = -5f;
-    }
 	
 	void Update () {
-        timer += Time.deltaTime;
-        if (timer < 0 || timer > totalTime || collided)
-        {
-            if (timer < 0)
-            {
-                gui.text = "wave your hands in " + Convert.ToInt32(0f - timer);
-            }
-            else if (timer > totalTime)
-            {
-                gui.text = "final score " + points;
-            }
-            GetComponent<Rigidbody>().velocity = Vector3.zero;
-            return;
-        }
-
-        float diff = left.transform.position.y - right.transform.position.y;
-
-        transform.Translate(Vector3.down * Time.deltaTime * downwardSpeed);
-        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
-        if (turn)
-        {
-            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * diff);
-            // transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + (Time.deltaTime * turnSpeed * diff),  diff * RotateRat));
-            dragon.transform.localRotation = Quaternion.Euler(diff * RotateRat * Vector3.forward);
-        }
-        else
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * strafeSpeed * diff);
-        }
-
-        Debug.Log("Rise " + Mathf.Max(left.HeightDiff, right.HeightDiff));
-
-        // Wave down
-        if (left.HeightDiff > 0f && right.HeightDiff > 0f)
-        {
-            float rise = Mathf.Min(left.HeightDiff, right.HeightDiff);
-            transform.Translate(Vector3.up * waveRiseRatio * rise);
-        }
-
         if (left.MenuPressed && right.MenuPressed)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collided");
-        collided = true;
-        gui.text = "CRASHED";
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "loop")
-            points += pointInc;
-        else
+        if (dragon.CanMove)
         {
+            float diff = left.transform.position.y - right.transform.position.y;
 
-            Debug.Log("Collided");
-            collided = true;
-            gui.text = "CRASHED";
+            transform.Translate(Vector3.down * Time.deltaTime * downwardSpeed);
+            transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+            if (turn)
+            {
+                transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * diff);
+                // transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + (Time.deltaTime * turnSpeed * diff),  diff * RotateRat));
+                dragon.transform.localRotation = Quaternion.Euler(diff * RotateRat * Vector3.forward);
+            }
+            else
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * strafeSpeed * diff);
+            }
+
+            Debug.Log("Rise " + Mathf.Max(left.HeightDiff, right.HeightDiff));
+
+            // Wave down
+            if (left.HeightDiff > 0f && right.HeightDiff > 0f)
+            {
+                float rise = Mathf.Min(left.HeightDiff, right.HeightDiff);
+                transform.Translate(Vector3.up * waveRiseRatio * rise);
+            }
         }
     }
 }
